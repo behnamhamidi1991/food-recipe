d9d7eab1ed82ac87d36d866775a52f8b5e1dafa7
Behnam Hamidi <eslp91@hotmail.com>
2024-03-17 23:41:46 +0330

    opacity

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,11 +1,49 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 export default function WelcomeScreen() {
   return (
-    <View>
-      <Text>Welcome</Text>
+    <View style={{ flex: 1, alignItems: "center", marginTop: 180 }}>
+      <Image
+        source={require("../../assets/images/main.webp")}
+        resizeMode="contain"
+        style={{ width: 370, height: 250 }}
+      />
+      <Text
+        style={{
+          color: "#f96163",
+          fontWeight: "bold",
+          marginTop: 40,
+          fontSize: 27,
+        }}
+      >
+        {" "}
+        40K+ Premium Recipes{" "}
+      </Text>
+      <Text
+        style={{
+          fontSize: 17,
+          fontWeight: "light",
+          color: "#3c444c",
+          marginTop: 5,
+          marginBottom: 20,
+        }}
+      >
+        Cook Like A Real Chef
+      </Text>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#f96163",
+          borderRadius: 18,
+          paddingVertical: 18,
+          width: "80%",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "#fff", fontWeight: "700" }}>Let's Go</Text>
+      </TouchableOpacity>
     </View>
   );
 }
